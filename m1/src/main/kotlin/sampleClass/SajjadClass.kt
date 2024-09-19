3e8e18cf6de8ee5888bc2ca2c4c497f02a8e83e4
sajjad <sajjadyami77@gmail.com>
2024-09-19 15:32:29 +0330

    [feature] now can read jar files and make an instance from it and print it from that class

---
+++ m1/src/main/kotlin/sampleClass/SajjadClass.kt
@@ -0,0 +1,10 @@
+package org.example.sampleClass
+
+class SajjadClass {
+    private val name = "Sajjad"
+    private val familyName = "Yami"
+    override fun toString(): String {
+        return "SajjadClass(name='$name', familyName='$familyName')"
+    }
+
+}
